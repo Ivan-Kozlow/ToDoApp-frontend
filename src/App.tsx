14edf0453030c352533a1798cb7c59ad95569d74
Ivan <ivan-kozlow04@mail.ru>
2023-08-26 16:44:21 +0300

    Merge project updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Sidebar } from './components/Sidebar'
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useAppDispatch } from './hooks/redux'
 import { userActions } from './redux/slices/user/userSlice'
 import userService from './services/user.service'
-
+import AuthPage from 'pages/AuthPage'
+import { Route, Routes } from 'react-router-dom'
+import MainPage from 'pages/MainPage'
+import PageNotFound from 'components/PageNotFound/PageNotFound'
 function App() {
 	const dispatch = useAppDispatch()
 	const { data } = useQuery({
@@ -16,8 +18,12 @@ function App() {
 	}, [data])
 
 	return (
-		<div className='bg-primary'>
-			<Sidebar />
+		<div>
+			<Routes>
+				<Route path='/' element={<MainPage />}></Route>
+				<Route path='/auth' element={<AuthPage />}></Route>
+				<Route path='/*' element={<PageNotFound />}></Route>
+			</Routes>
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.scss'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient({
 	defaultOptions: { queries: { keepPreviousData: true, refetchOnWindowFocus: false } },
@@ -12,7 +13,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</QueryClientProvider>
 	</Provider>
 )
