6b387227167892be210a132da0b7cbf293e46512
Dzasur <alimahmadovdzasur@gmail.com>
2023-08-27 11:18:09 +0300

    added new file Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useAppSelector } from '../hooks/redux'
-import { RootState } from 'redux/store'
+import { RootState } from '../redux/store'
 
 const Header = () => {
-	const { nickname } = useAppSelector((state):RootState => state.user)
+	// const { nickname } = useAppSelector((state):RootState => state.user)
 	return (
 		<div>
-			<h1>Welcome back, {nickname}</h1>
+			{/* <h1>Welcome back, {nickname}</h1> */}
 		</div>
 	)
 }
