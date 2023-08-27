4f711682d772d1a71e93a096abdea203fc8b154c
Ivan <ivan-kozlow04@mail.ru>
2023-08-27 11:11:40 +0300

    starting create authPage

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,11 @@
+import FormRegister from 'components/auth/FormRegister'
+
+const AuthPage = () => {
+	return (
+		<div>
+			<FormRegister></FormRegister>
+		</div>
+	)
+}
+
+export default AuthPage
