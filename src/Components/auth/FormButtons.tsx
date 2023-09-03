04aa43552e1c93a4dd677b14c6dd613b3028bf02
Ivan <ivan-kozlow04@mail.ru>
2023-09-03 17:45:21 +0300

    add switch auth Now can change registration for login and vice versa

--- src/Components/auth/FormButtons.tsx
+++ src/Components/auth/FormButtons.tsx
@@ -1,39 +1,78 @@
+import React from 'react'
 import style from './FormRegister.module.scss'
-
-import { Button } from '@mui/material'
-import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded'
 import { useNavigate } from 'react-router-dom'
 
-const FormButtons: React.FC<{ buttonText: string }> = ({ buttonText }) => {
+import { Button, Popover } from '@mui/material'
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+const FormButtons: React.FC<{ buttonText: 'Зарегистрироваться' | 'Войти' }> = ({ buttonText }) => {
+	const [open, setOpen] = React.useState(false)
+	const btnRef = React.useRef<HTMLButtonElement>(null)
 	const navigate = useNavigate()
 
+	const inverseButtonText = buttonText === 'Зарегистрироваться' ? 'Войти' : 'Зарегистрироваться'
 	const isSmallButton = buttonText.length <= 7
-
-	const getFontSize = () => {
-		return `${isSmallButton ? '14px' : '12px'}`
+	const btnStyle = {
+		padding: `${isSmallButton ? '7px 12px' : '8px 8px'}`,
+		fontSize: `${isSmallButton ? '14px' : '12px'}`,
 	}
 
 	return (
 		<div className={style.buttons}>
 			<Button
-				sx={{
-					padding: `${isSmallButton ? '7px 12px' : '8px 8px'}`,
-					fontSize: getFontSize(),
-				}}
+				title='На главную'
+				sx={btnStyle}
 				startIcon={isSmallButton ? <ArrowBackRoundedIcon fontSize='small' /> : ''}
 				onClick={() => navigate('/')}
 			>
 				Главая
 			</Button>
-			<Button
-				sx={{
-					padding: `${isSmallButton ? '7px 15px' : '8px 8px'}`,
-					fontSize: getFontSize(),
-				}}
-				type='submit'
-			>
-				{buttonText}
-			</Button>
+
+			<div>
+				<Button title={buttonText} sx={btnStyle} type='submit'>
+					{buttonText}
+				</Button>
+				<button
+					ref={btnRef}
+					className='px-[4px] py-[7px] ml-2'
+					title={inverseButtonText}
+					type='button'
+					onClick={() => setOpen(true)}
+				>
+					{open ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+				</button>
+				<Popover
+					anchorEl={btnRef.current}
+					open={open}
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'right',
+					}}
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					onClose={() => setOpen(false)}
+					sx={{ marginTop: '10px' }}
+				>
+					<Button
+						sx={{
+							backgroundColor: 'rgba(0, 0, 0, 0.8)',
+							color: 'white',
+							':hover': { backgroundColor: 'rgba(0, 0, 0, 0.7)' },
+						}}
+						variant='outlined'
+						onClick={() => {
+							setOpen(false)
+							navigate(`${buttonText === 'Войти' ? '/auth/register' : '/auth/login'}`)
+						}}
+					>
+						{inverseButtonText}
+					</Button>
+				</Popover>
+			</div>
 		</div>
 	)
 }
