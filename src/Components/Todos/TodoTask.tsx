16e22002e491b0fb6c0e2db9ddfc9331708e2a30
Ivan <ivan-kozlow04@mail.ru>
2023-10-15 13:38:06 +0300

    add mutate on primary task btn

--- src/Components/Todos/TodoTask.tsx
+++ src/Components/Todos/TodoTask.tsx
@@ -1,8 +1,11 @@
 import { FC, useState } from 'react'
-import { useAppDispatch } from 'hooks/redux'
-import { getLocalDateNumbers } from 'utils/getLocalDate'
+import { useMutation } from '@tanstack/react-query'
 import { ITodo } from 'Redux/slices/todo/typesTodo'
 import { todoActions } from 'Redux/slices/todo/todoSlice'
+import { useAppDispatch } from 'hooks/redux'
+import { getLocalDateNumbers } from 'utils/getLocalDate'
+import { keyTodoUpdate } from 'consts/queryKeys'
+import todoService from 'services/todo.service'
 
 import MoreTodoMenu from 'components/Todos/MorePopover'
 import TodoProgressBar from './Form/TodoProgressBar'
@@ -15,9 +18,18 @@ type TypeTodoTaskProps = Pick<ITodo, 'title' | 'body' | 'completed' | 'createdAt
 const TodoTask: FC<TypeTodoTaskProps> = ({ title, body, completed, createdAt, _id }) => {
 	const [createTask, setCreateTask] = useState(false)
 	const dispatch = useAppDispatch()
+	const { mutate } = useMutation({
+		mutationKey: [keyTodoUpdate],
+		mutationFn: () =>
+			completed < 2
+				? todoService.update(_id, { completed: ++completed as ITodo['completed'] })
+				: todoService.delete(_id),
+		onSuccess() {
+			completed < 2 ? dispatch(todoActions.updateStatus(_id)) : dispatch(todoActions.deleteTask(_id))
+		},
+	})
 
 	const date = getLocalDateNumbers(createdAt)
-
 	return (
 		<article className='bg-taskBox rounded-md p-5'>
 			{createTask && (
@@ -47,11 +59,11 @@ const TodoTask: FC<TypeTodoTaskProps> = ({ title, body, completed, createdAt, _i
 					<div className='flex items-center justify-between'>
 						<p className='todo-text px-4 py-2 bg-[#FFFFFF0F] rounded-full'>{date}</p>
 						{completed === 2 ? (
-							<button onClick={() => dispatch(todoActions.deleteTask(_id))} title='Удалить'>
+							<button onClick={() => mutate()} title='Удалить'>
 								<RemoveCircleOutlineOutlinedIcon />
 							</button>
 						) : (
-							<button onClick={() => dispatch(todoActions.updateStatus(_id))} title='Выполнить'>
+							<button onClick={() => mutate()} title='Выполнить'>
 								<CheckCircleOutlineIcon />
 							</button>
 						)}
