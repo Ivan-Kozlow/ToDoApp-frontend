557799ddcaeae9ebf809c87b1935a0069fc7c5db
Ivan <ivan-kozlow04@mail.ru>
2024-01-16 12:18:18 +0300

    change alias component (vite.config)

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
-			components: '/src/Components/',
+			Components: '/src/Components/',
 			hooks: '/src/hooks/',
 			pages: '/src/pages/',
 			Redux: '/src/redux/',
